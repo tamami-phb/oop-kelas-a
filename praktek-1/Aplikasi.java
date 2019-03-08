5e690c52265360a06ca173f4e134c36ecd399eb1
tamami <tamami.oka@gmail.com>
2019-03-08 14:10:24 +0700

    init commit praktek-1

---
+++ praktek-1/Aplikasi.java
@@ -0,0 +1,18 @@
+public class Aplikasi {
+    public static void main(String args[]) {
+        Mahasiswa mhs1 = 
+            new Mahasiswa("19001", "tamami");
+        Mahasiswa mhs2 = 
+            new Mahasiswa("19002", "oka");
+
+        mhs1.cetak();
+        mhs2.cetak();
+        System.out.println(mhs1.getNim());
+        //mhs1.nama = "tamami";
+        //mhs1.nim = "19001";
+        //System.out.println(mhs1.nim + " : " + 
+        //  mhs1.nama);
+        //System.out.println(mhs2.nim + " : " + 
+        //  mhs2.nama);
+    }
+}
