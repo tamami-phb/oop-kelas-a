fe99de76a3cee15e4707bdcd0fe30727f4308a36
tamami <tamami.oka@gmail.com>
2019-04-05 14:11:51 +0700

    create MahasiswaService

---
+++ project-mahasiswa/services/MahasiswaService.java
@@ -0,0 +1,27 @@
+package services;
+
+public class MahasiswaService {
+
+    private static List<Mahasiswa> data = new LinkedList<Mahasiswa>();
+
+    public void tambahData(Mahasiswa mhs) {
+        data.add(mhs);
+        System.out.println("data telah tersimpan");
+    }
+
+    public void ubahData(Mahasiswa mhs) {}
+
+    public void hapusData(String nim) {}
+
+    public void tampilkanData() {
+        System.out.println("\n--= Data Mahasiswa =--");
+        int urutan = 1;
+        for (Mahasiswa mhs : data){
+            System.out.println("data ke-" + urutan++);
+            System.out.println("  NIM : " + mhs.getNim());
+            System.out.println("  NAMA : " + mhs.getNama());
+            System.out.println("  KELAS : " + mhs.getKelas());
+        }
+    }
+
+}
