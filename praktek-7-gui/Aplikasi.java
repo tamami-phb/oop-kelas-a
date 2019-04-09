5ea61b76fbe00203c87d7037105bc412db7df16c
tamami <tamami.oka@gmail.com>
2019-04-09 14:17:22 +0700

    update gui

--- praktek-7-gui/Aplikasi.java
+++ praktek-7-gui/Aplikasi.java
@@ -3,13 +3,29 @@ import javax.swing.*;
 
 public class Aplikasi {
 
-    static GraphicsConfiguration gc;
-
     public static void main(String[] args) {
-        JFrame app = new JFrame(gc);
-        app.setTitle("Test Jendela");
+        JFrame app = new JFrame("Test Jendela");
+
+        JLabel lblNim = new JLabel("NIM");
+        JLabel lblNama = new JLabel("NAMA");
+        JLabel lblKelas = new JLabel("KELAS");
+        JTextField txtNim = new JTextField();
+        JTextField txtNama = new JTextField();
+        JTextField txtKelas = new JTextField();
+        JButton btn = new JButton("Simpan");
+        JPanel panel = new JPanel();
+
+        panel.setLayout(new GridLayout(3, 2));
+        panel.add(lblNim); panel.add(txtNim);
+        panel.add(lblNama); panel.add(txtNama);
+        panel.add(lblKelas); panel.add(txtKelas);
+
+        app.getContentPane().add(panel, BorderLayout.CENTER);
+        app.getContentPane().add(btn, BorderLayout.SOUTH);
+
         app.setSize(300, 200);
         app.setVisible(true);
+        app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
     }
 
 }
