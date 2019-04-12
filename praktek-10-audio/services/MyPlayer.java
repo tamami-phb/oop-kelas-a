b61009b54f7a17c0b9fb4ec5a1a3418b4cd28252
tamami <tamami.oka@gmail.com>
2019-04-12 14:58:55 +0700

    audio player: ok

--- praktek-10-audio/services/MyPlayer.java
+++ praktek-10-audio/services/MyPlayer.java
@@ -7,6 +7,8 @@ public class MyPlayer {
 
     private AudioInputStream ais;
     private Clip clip;
+    private String filePath;
+    private Long currentPosition;
 
     public MyPlayer(String filePath) throws
             UnsupportedAudioFileException,
@@ -14,20 +16,41 @@ public class MyPlayer {
         ais = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
         clip = AudioSystem.getClip();
         clip.open(ais);
+        this.filePath = filePath;
     }
 
     public void play() {
         clip.start();
     }
 
-    public void pause() {}
-
-    public void resume() {}
-
-    public void stop() {
+    public void pause() {
+        currentPosition = clip.getMicrosecondPosition();
         clip.stop();
+        clip.close();
     }
 
-    public void reset() {}
+    public void resume() throws
+            UnsupportedAudioFileException,
+            LineUnavailableException, IOException {
+        reset();
+        clip.setMicrosecondPosition(currentPosition);
+        clip.start();
+    }
+
+    public void stop() throws
+            UnsupportedAudioFileException,
+            LineUnavailableException, IOException {
+        clip.stop();
+        clip.close();
+        reset();
+    }
+
+    public void reset() throws
+            UnsupportedAudioFileException,
+            LineUnavailableException, IOException {
+        ais = AudioSystem.getAudioInputStream(new File(filePath).getAbsoluteFile());
+        clip = AudioSystem.getClip();
+        clip.open(ais);
+    }
 
 }
--- praktek-10-audio/ui/MainUI.java
+++ praktek-10-audio/ui/MainUI.java
@@ -11,6 +11,8 @@ public class MainUI extends JFrame {
 
     private JButton btnPlay;
     private JButton btnStop;
+    private JButton btnPause;
+    private JButton btnResume;
     private Container contentPane;
     private MyPlayer player;
 
@@ -35,11 +37,18 @@ public class MainUI extends JFrame {
     private void initUI() {
         btnPlay = new JButton("Putar");
         btnStop = new JButton("Hentikan");
+        btnPause = new JButton("Jeda");
+        btnResume = new JButton("Lanjutkan");
 
         contentPane = getContentPane();
         contentPane.setLayout(new FlowLayout(FlowLayout.LEFT));
         contentPane.add(btnPlay);
         contentPane.add(btnStop);
+        contentPane.add(btnPause);
+        contentPane.add(btnResume);
+        btnStop.setEnabled(false);
+        btnPause.setEnabled(false);
+        btnResume.setEnabled(false);
 
         pack();
         setVisible(true);
@@ -47,20 +56,76 @@ public class MainUI extends JFrame {
 
         btnPlay.addActionListener(new BtnPlayClick());
         btnStop.addActionListener(new BtnStopClick());
+        btnPause.addActionListener(new BtnPauseClick());
+        btnResume.addActionListener(new BtnResumeClick());
     }
 
 
     // ------- events
 
+    class BtnPauseClick implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            player.pause();
+            btnPlay.setEnabled(false);
+            btnPause.setEnabled(false);
+            btnStop.setEnabled(true);
+            btnResume.setEnabled(true);
+        }
+    }
+
+    class BtnResumeClick implements ActionListener {
+        public void actionPerformed(ActionEvent evt) {
+            try {
+                player.resume();
+            } catch(UnsupportedAudioFileException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Kesalahan",
+                        "berkas tersebut tidak didukung",
+                        JOptionPane.ERROR_MESSAGE);
+            } catch(LineUnavailableException e) {
+                JOptionPane.showMessageDialog(null,
+                        "Hubungi developer");
+            } catch(IOException e) {
+                JOptionPane.showMessageDialog(null,
+                        "File tidak bisa diakses");
+            }
+            btnResume.setEnabled(false);
+            btnPlay.setEnabled(false);
+            btnPause.setEnabled(true);
+            btnStop.setEnabled(true);
+        }
+    }
+
     class BtnPlayClick implements ActionListener {
         public void actionPerformed(ActionEvent evt) {
             player.play();
+            btnStop.setEnabled(true);
+            btnPause.setEnabled(true);
+            btnResume.setEnabled(false);
+            btnPlay.setEnabled(false);
         }
     }
 
     class BtnStopClick implements ActionListener {
         public void actionPerformed(ActionEvent evt) {
-            player.stop();
+            try {
+                player.stop();
+            } catch(UnsupportedAudioFileException e) {
+                JOptionPane.showMessageDialog(null,
+                    "Kesalahan",
+                    "berkas tersebut tidak didukung",
+                    JOptionPane.ERROR_MESSAGE);
+            } catch(LineUnavailableException e) {
+                JOptionPane.showMessageDialog(null,
+                    "Hubungi developer");
+            } catch(IOException e) {
+                JOptionPane.showMessageDialog(null,
+                    "File tidak bisa diakses");
+            }
+            btnStop.setEnabled(false);
+            btnPause.setEnabled(false);
+            btnResume.setEnabled(false);
+            btnPlay.setEnabled(true);
         }
     }
 
